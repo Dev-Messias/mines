5cc0874daab20857b248e23f444ae1e40bc901f2
MessiasLimaE <messias1817@gmail.com>
2019-07-09 20:48:26 -0300

    04 - Componente Field

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import params from './src/params'
+import Field from './src/components/Field'
 
 
 export default class App extends Component {
@@ -10,6 +11,16 @@ export default class App extends Component {
         <Text style={styles.welcome}>Iniciando o Mines!</Text>
         <Text style={styles.instructions}>Tamanho da grade: 
         {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+
+        <Field/>
+        <Field opened />
+        <Field  opened nearMines={1}/>
+        <Field  opened nearMines={2}/>
+        <Field  opened nearMines={3}/>
+        <Field  opened nearMines={6}/>
+        <Field  mined />
+        <Field  mined opened />
+        <Field  mined opened exploded />
       </View>
     );
   }
